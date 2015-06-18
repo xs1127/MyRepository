0807ddae5a2d86b505a7d7c53cf67a576ed20b9d
xs1127 <sunms1127@163.com>
2015-06-18 12:21:02 +0800

    add project

---
+++ CrashLog/src/Stream.java
@@ -0,0 +1,17 @@
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+
+public class Stream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		InputStream inputStream ;
+		BufferedInputStream inputStream2;
+		ByteArrayInputStream inputStream3;
+		String string;
+	}
+
+}
